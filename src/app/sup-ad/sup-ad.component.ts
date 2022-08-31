de2cacfca15661ff2147aa32672485be2f8c404d
pakaok <jaeseok.kim@griffithuni.edu.au>
2022-08-31 19:49:33 +1000

    html admin part design  done

--- src/app/sup-ad/sup-ad.component.ts
+++ src/app/sup-ad/sup-ad.component.ts
@@ -8,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class SupAdComponent implements OnInit {
   vv=false
   user_list=['one','two']
+  group_list=['group1','group2']
+  channel_list=['channel1','channel2']
   constructor() { }
-  color=1
+  state=3
   ngOnInit(): void {
   }
-
+  chan(x:number){
+    this.state = x
+  }
 }
