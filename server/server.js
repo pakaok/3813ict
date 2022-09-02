5255840d0a780ac89860b5fcb779317ef42247c0
pakaok <jaeseok.kim@griffithuni.edu.au>
2022-09-02 15:45:15 +1000

    server implemented

---
+++ server/server.js
@@ -0,0 +1,62 @@
+var express = require('express')
+var app = express()
+var cors = require('cors')
+var http = require('http').Server(app)
+const io = require('socket.io')(http,{
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ['GET','POST']
+    }
+})
+
+app.use(cors())
+
+io.on('connection',(socket)=>{
+    console.log('Connected on server.js'+',  '+socket.id)
+
+    socket.on('msg',(msg)=>{
+        io.emit('msg',msg)
+    })
+})
+
+http.listen(3000,()=>{
+    var d = new Date()
+    var n = d.getHours()
+    var m = d.getMinutes()
+    console.log('Server has been started at : ' + n + ' : '+ m)
+})
+
+
+
+
+// var server = http.listen(3000,function(){
+//     console.log('server listening')
+// })
+
+// app.post('/api/auth',function(req,res){
+//     let users= [{username:'Al',birthdate:'1900/01/1',age:'3',email:'a', password:'s',valid:false}]
+
+//     if (!req.body){
+//         return res.sendStatus(400)
+//     }
+
+//     var customer = {}
+//     customer.email=req.body.email
+//     customer.password=req.body.password
+//     customer.valid = false
+//     console.log(customer)
+//     find =  users.some(e => (e.email==customer.email&&e.password==customer.password))
+    
+//         if (find){
+//             users.forEach(e=>{
+//                 if (e.email==customer.email&&e.password==customer.password){
+//                 customer = e
+//                 customer.valid = true
+//             }
+//         })
+//         }
+    
+    
+//     res.send(customer)
+
+// })
